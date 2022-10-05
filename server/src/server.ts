c34befda3eca52c31c0ed306c63834939cd946e5
Sonu Shahuji <sonushahuji4@gmail.com>
2022-10-05 21:38:21 +0530

    added endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -28,7 +28,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000/",
+      origin: process.env.CLENT_ENDPOINT,
       methods: ["GET", "POST"],
     },
   });
